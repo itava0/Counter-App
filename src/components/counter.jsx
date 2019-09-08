e92616434eddf65cfb9300343022ffe72e590c2e
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-09-07 20:27:26 -0400

    Counter App

---
+++ src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hellow World</h1>
+        <button>Incremen</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
