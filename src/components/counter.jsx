3324d5eee253b1eeb9a4f1fda59c055f881cf0b4
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-09-28 19:08:11 -0400

    Added functionaly to delete each counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,25 +2,11 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: this.props.value
-    // tags: ['tag1', 'tag2', 'tag3']
+    value: this.props.counter.value
   };
-  // styles = { We could Pass classes dynamically in to our element
-  //   fontSize:10,
-  //   fontWeight: "Bold"
-  // };
-  constructor(props) {
-    super(props);
-    this.handleIncrement = this.handleIncrement.bind(this);
-  }
 
-  handleIncrement(product) {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
-  }
-
-  doHandleIncrement = () => {
-    this.handleIncrement({ id: 1 });
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
@@ -34,28 +20,24 @@ class Counter extends Component {
           Increment
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
         </button>
-        {/* <ul>
-          {/* We could loop through a list of itmes and render them dynamically on the page 
-          {this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}
-        </ul> */}
       </div>
     );
   }
   // WE could extract in separate method with ctrl + shift + r
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,21 +3,25 @@ import Counter from "./counter";
 
 class Counters extends Component {
   state = {
-    counters: [{ value: 4 }, { value: 0 }, { value: 0 }, { value: 0 }]
+    counters: [
+      { id: 1, value: 4 }, 
+      { id: 2, value: 0 }, 
+      { id: 3, value: 0 }, 
+      { id: 4, value: 0 }]
   };
 
   handleDelete = counterId => {
-    console.log("Event Handler Called", counterId);
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({counters})
   };
   render() {
     return (
       <div>
-        {this.state.counters.map((counter, id) => (
+        {this.state.counters.map(counter => (
           <Counter
-            key={id}
+            key={counter.id}
             onDelete={this.handleDelete}
-            value={counter.value}
-            id={id}
+            counter={counter}
           />
         ))}
       </div>
