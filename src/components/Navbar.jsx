57a9bb59351f2c39b7e738ba2bc05f05ed0d2253
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-09-29 16:02:25 -0400

    Converted the Navbar file to a functional component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
-      </nav>
-    );
-  }
-}
+const Navbar = ({totalCounters}) => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default Navbar;
